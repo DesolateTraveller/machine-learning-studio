35329550298c696e9c2cb76125c4b73c20ca103f
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-07-26 10:45:33 +0530

    Update ml_studio.py

--- ml_studio.py
+++ ml_studio.py
@@ -93,7 +93,7 @@ st.set_page_config(page_title="ML Studio",
                    #page_icon=               
                    initial_sidebar_state="auto")
 #----------------------------------------
-st.title(f""":rainbow[Machine Learning (ML) Studio | v1.0]""")
+st.title(f""":rainbow[Machine Learning (ML) Studio | v0.1]""")
 st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
             unsafe_allow_html=True)
 #st.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
