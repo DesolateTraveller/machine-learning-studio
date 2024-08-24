0029206acde8d3ba86bdb674185ee7153531380e
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-08-24 15:21:28 +0530

    Update ml_studio.py

--- ml_studio.py
+++ ml_studio.py
@@ -88,12 +88,12 @@ from pycaret.classification import setup, compare_models, predict_model, pull, p
 ### Title and description for your Streamlit app
 #---------------------------------------------------------------------------------------------------------------------------------
 #import custom_style()
-st.set_page_config(page_title="ML Studio",
+st.set_page_config(page_title="ML Studio | v0.1",
                    layout="wide",
                    #page_icon=               
                    initial_sidebar_state="auto")
 #----------------------------------------
-st.title(f""":rainbow[Machine Learning (ML) Studio | v0.1]""")
+st.title(f""":rainbow[Machine Learning (ML) Studio]""")
 st.markdown('Created by | <a href="mailto:avijit.mba18@gmail.com">Avijit Chakraborty</a>', 
             unsafe_allow_html=True)
 #st.info('**Disclaimer : :blue[Thank you for visiting the app] | Unauthorized uses or copying of the app is strictly prohibited | Click the :blue[sidebar] to follow the instructions to start the applications.**', icon="ℹ️")
