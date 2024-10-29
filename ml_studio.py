52f9b48fcd2f420bfc89e3766aae5621d3463f1f
Avijit Chakraborty <74238071+DesolateTraveller@users.noreply.github.com>
2024-10-29 09:56:34 +0530

    Update ml_studio.py

--- ml_studio.py
+++ ml_studio.py
@@ -767,7 +767,7 @@ else:
                                         best_model.fit(X_train, y_train)
                                         y_pred_best = best_model.predict(X_test)
                                         y_proba_best = best_model.predict_proba(X_test)[:, 1] if hasattr(best_model, "predict_proba") else None                                        
-                                        st.sidebar.info(f"Best model : **{best_model_clf}**")
+                                        #st.sidebar.info(f"Best model : **{best_model_clf}**")
 
                             with col2:
                                     
@@ -784,7 +784,6 @@ else:
                                     plt.legend(title="Model", bbox_to_anchor=(1.05,1), loc='upper left')
                                     st.pyplot(plt, use_container_width=True)
     
-
                                     #st.subheader("Hyperparameters",divider='blue')
                                     #if isinstance(best_model, LogisticRegression):
                                         #st.write(f"**C**: {C}")
@@ -825,7 +824,7 @@ else:
                                         best_model.fit(X_train, y_train)
                                         y_pred_best = best_model.predict(X_test)
                                         y_proba_best = best_model.predict_proba(X_test) if hasattr(best_model, "predict_proba") else None
-                                        st.sidebar.info(f"Best model : **{best_model_clf}**")
+                                        #st.sidebar.info(f"Best model : **{best_model_clf}**")
 
                             with col2:
 
@@ -861,15 +860,21 @@ else:
                     st.subheader("Importance",divider='blue')
 
                     if best_model_clf == "Logistic Regression":
-                        importance = best_model.coef_.flatten()
+                        #importance = best_model.coef_.flatten()
+                        importance_df = pd.DataFrame({'Feature': X.columns, 'Coefficient': best_model.coef_.flatten()})
+                        importance_df['Percentage'] = (abs(importance_df['Coefficient']) / abs(importance_df['Coefficient']).sum()) * 100
+                        importance_df = importance_df.sort_values(by='Coefficient', ascending=False)
                     else:
-                        importance = best_model.feature_importances_
+                        #importance = best_model.feature_importances_
+                        importance_df = pd.DataFrame({"Feature": selected_features, "Importance": best_model.feature_importances_})
+                        importance_df['Percentage'] = (importance_df['Importance'] / importance_df['Importance'].sum()) * 100
+                        importance_df = importance_df.sort_values(by='Importance', ascending=False)
 
                     col1, col2 = st.columns((0.15,0.85))
                     with col1:
                         with st.container():
 
-                            importance_df = pd.DataFrame({"Feature": selected_features,"Importance": importance})
+                            #importance_df = pd.DataFrame({"Feature": selected_features,"Importance": importance})
                             st.dataframe(importance_df, hide_index=True, use_container_width=True)
 
                     with col2:
@@ -880,6 +885,30 @@ else:
                             fig = go.Figure(data = plot_data_imp, layout = plot_layout_imp)
                             st.plotly_chart(fig,use_container_width = True)
 
+                    st.sidebar.divider()
+                    st.sidebar.info(f"Best model : **{best_model_clf}**")
+                    if best_model_clf == "Logistic Regression":
+                        stats_expander = st.sidebar.expander("**:blue[Important features]**", expanded=False)
+                        with stats_expander:
+                            #st.info(f"**Top Features based on Importance:**\n1. {importance_df.iloc[0]['Feature']}\n2. {importance_df.iloc[1]['Feature']}\n3. {importance_df.iloc[2]['Feature']}\n4. {importance_df.iloc[3]['Feature']}\n5. {importance_df.iloc[4]['Feature']}")
+                            st.info(f"**Top Features based on Importance:**\n"
+                                    f"1. {importance_df.iloc[0]['Feature']} ({importance_df.iloc[0]['Percentage']:.2f}%)\n"
+                                    f"2. {importance_df.iloc[1]['Feature']} ({importance_df.iloc[1]['Percentage']:.2f}%)\n"
+                                    f"3. {importance_df.iloc[2]['Feature']} ({importance_df.iloc[2]['Percentage']:.2f}%)\n"
+                                    f"4. {importance_df.iloc[3]['Feature']} ({importance_df.iloc[3]['Percentage']:.2f}%)\n"
+                                    f"5. {importance_df.iloc[4]['Feature']} ({importance_df.iloc[4]['Percentage']:.2f}%)"
+                                        )           
+                    else:
+                        stats_expander = st.sidebar.expander("**:blue[Important features]**", expanded=False)
+                        with stats_expander:
+                            #st.info(f"**Top Features based on Importance:**\n1. {importance_df.iloc[0]['Feature']}\n2. {importance_df.iloc[1]['Feature']}\n3. {importance_df.iloc[2]['Feature']}\n4. {importance_df.iloc[3]['Feature']}\n5. {importance_df.iloc[4]['Feature']}")
+                            st.info(f"**Top Features based on Importance:**\n"
+                                    f"1. {importance_df.iloc[0]['Feature']} ({importance_df.iloc[0]['Percentage']:.2f}%)\n"
+                                    f"2. {importance_df.iloc[1]['Feature']} ({importance_df.iloc[1]['Percentage']:.2f}%)\n"
+                                    f"3. {importance_df.iloc[2]['Feature']} ({importance_df.iloc[2]['Percentage']:.2f}%)\n"
+                                    f"4. {importance_df.iloc[3]['Feature']} ({importance_df.iloc[3]['Percentage']:.2f}%)\n"
+                                    f"5. {importance_df.iloc[4]['Feature']} ({importance_df.iloc[4]['Percentage']:.2f}%)"
+                                        )
             #----------------------------------------                
                 if ml_type == 'Regression': 
 
